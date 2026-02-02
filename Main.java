88121f38518347b199b2d4edef1f6496edfb8b88
starwald <mal25717@uvg.edu.gt>
2026-02-02 10:45:16 -0600

    Corrección de declaración de paquetes (inexistente)

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package Test;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- PostfixCalculatorTest.java
+++ PostfixCalculatorTest.java
@@ -1,5 +1,3 @@
-package Test;
-
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
